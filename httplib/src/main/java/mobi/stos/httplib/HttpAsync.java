64b4f334d42ea2b31cca0e3ec8f8b7a7263296a7
Weibson S'tos <weibson@stos.mobi>
2018-08-28 15:24:46 -0300

    Commit 1.0.5 Ajustes de execução serial

--- httplib/src/main/java/mobi/stos/httplib/HttpAsync.java
+++ httplib/src/main/java/mobi/stos/httplib/HttpAsync.java
@@ -1,5 +1,7 @@
 package mobi.stos.httplib;
 
+import android.os.AsyncTask;
+
 import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
@@ -18,6 +20,7 @@ public class HttpAsync implements RestComposer {
 
     private boolean debug = false;
     private boolean aceitarCertificadoInvalido = false;
+    private boolean execucaoSerial = true;
 
     private final URL url;
     private final Map<String, Object> params;
@@ -55,6 +58,34 @@ public class HttpAsync implements RestComposer {
         return this;
     }
 
+    /**
+     * <p>A execução serial será por padrão TRUE.</p>
+     * <p>Significa que existirá somente uma tarefa rodando em forma serial. (Uma tarefa acaba para
+     * executar a próxima tarefa).</p>
+     * <p>Se essa opção for marcado como FALSE a execução acontecerá de forma não serial, ou seja,
+     * paralelas.</p>
+     *
+     * <p>
+     * <strong>Aviso:</strong><br>
+     * Permitir que várias tarefas sejam executadas em paralelo a partir de um conjunto de
+     * encadeamentos geralmente não é o que se deseja, porque a ordem de sua operação não está
+     * definida. <br><br>
+     *
+     * Por exemplo, se essas tarefas forem usadas para modificar qualquer estado em comum (como
+     * gravar um arquivo devido a um clique no botão), não há garantias sobre a ordem das
+     * modificações. Sem um trabalho cuidadoso, é possível, em casos raros, que a versão mais
+     * recente dos dados seja sobreposta por uma mais antiga, levando a problemas de perda de dados
+     * e estabilidade. Tais mudanças são melhor executadas em serial; Para garantir que esse
+     * trabalho seja serializado, independentemente da versão da plataforma, você pode usar essa
+     * função com TRUE.
+     * </p>
+     *
+     * @param execucaoSerial Boolean
+     */
+    public void setExecucaoSerial(boolean execucaoSerial) {
+        this.execucaoSerial = execucaoSerial;
+    }
+
     public void setAceitarCertificadoInvalido(boolean aceitarCertificadoInvalido) {
         this.aceitarCertificadoInvalido = aceitarCertificadoInvalido;
     }
@@ -67,7 +98,7 @@ public class HttpAsync implements RestComposer {
         task.setCallback(callback);
         task.setDebug(this.debug);
         task.setTrustAllCerts(this.aceitarCertificadoInvalido);
-        task.execute();
+        task.executeOnExecutor(execucaoSerial ? AsyncTask.SERIAL_EXECUTOR : AsyncTask.THREAD_POOL_EXECUTOR);
     }
 
     @Override
